123e139a7c04125e2a27e51a300570b1dbebbd58
M <1163182715@qq.com>
2018-06-06 22:14:02 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  import * as THREE from 'three'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+
+    }
+  },
+  methods(){
+    a(){} {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>

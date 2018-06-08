3d3c02cfb4d09c93799fdaad60d4d308cba51b3f
M <1163182715@qq.com>
2018-06-08 19:29:34 +0800

    update

---
+++ example/test-cube/utils/color.js
@@ -0,0 +1,7 @@
+var Goldenrod = "rgba(255,193,37,1)";
+var DeepSkyBlue = "rgba(0,191,255,1)";
+var White = "rgba(255,255,255,1)";
+var Black = "rgba(0,0,0,1)";
+var LimeGreen = "rgba(50,205,50,1)";
+var Firebrick = "rgba(178,34,34,1)";
+var Yellow = "rgba(255,255,0,1)";

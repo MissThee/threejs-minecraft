f6b1b73041a4c4d15b833b17fdfcf23bdd967368
MissThee <1163182715@qq.com>
2022-07-11 17:45:10 +0800

    调整打包配置

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,17 @@ export default defineConfig({
         }
     },
     build: {
-        outDir:'docs', // 配合github静态页的导出目录
+        outDir: 'docs', // 配合github静态页的导出目录
         assetsInlineLimit: 0,
+        rollupOptions: {
+            output: {
+                // 用于从入口点创建的块的打包输出格式[name]表示文件名,[hash]表示该文件内容hash值
+                entryFileNames: 'entry/[name].[hash].js',
+                // 用于命名代码拆分时创建的共享块的输出命名
+                chunkFileNames: 'chunk/[name].[hash].js',
+                // 用于输出静态资源的命名，[ext]表示文件扩展名
+                assetFileNames: 'assets/[ext]/[name].[hash].[ext]',
+            }
+        }
     }
 })
